eb4a451e77788ed2139699dd681e753df6bf9ec7
h1rosaka <hsakamoto111@gmail.com>
2025-01-03 17:28:43 +0900

    initial commit

---
+++ triangle.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+
+
+int main(){
+    float base , height;
+    float  area;
+
+    base = 3.0;
+    height = 3.0;
+
+    area = base * height / 2.0 ;
+
+    printf( "traiangle area %f\n", area);
+
+    return 0;
+}
